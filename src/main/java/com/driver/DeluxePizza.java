0d5a072f81c73ac21a277961cec558a303582c0d
unknown <patilvijaysing2092@gmail.com>
2023-04-14 17:44:43 +0530

    pizza delivery

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,12 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        // your code goes here
+        if (isVeg) {
+            this.addExtraCheese();
+            this.addExtraToppings();
+        } else {
+            this.addExtraCheese();
+            this.addExtraToppings();
+        }
     }
 }
